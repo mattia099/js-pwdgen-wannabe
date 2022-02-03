56bcdbeafed37acda2b949866c1b5b8dae87fd0a
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-02-03 16:39:54 +0100

    Corretto

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 
-const name= prompt('Inserire nome')
-const surname= prompt('Inserisci cognome')
-const color= prompt('Inserisci colore preferito')
-
+const name = prompt('Inserire nome')
+const surname = prompt('Inserisci cognome')
+const color = prompt('Inserisci colore preferito')
+const title = document.getElementById('title')
 title.innerHTML +=` ${name}${surname}${color}21`
