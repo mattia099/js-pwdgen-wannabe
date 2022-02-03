07ac21b010079c1295ddf693e38b8a6f99d4a858
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-02-03 14:57:35 +0100

    Completo

---
+++ javascript.js
@@ -0,0 +1,7 @@
+
+
+const name= prompt('Inserire nome')
+const surname= prompt('Inserisci cognome')
+const color= prompt('Inserisci colore preferito')
+
+title.innerHTML +=` ${name}${surname}${color}21`
